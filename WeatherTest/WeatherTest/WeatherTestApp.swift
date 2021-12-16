b184b4d3f962b603494d161903fa87de67859fe7
Ari Steinfeld <56319363+Not-AriStienfeld@users.noreply.github.com>
2021-12-16 09:21:16 -0500

    test

---
+++ WeatherTest/WeatherTest/WeatherTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTestApp.swift
+//  WeatherTest
+//
+//  Created by Ari Steinfeld (student LM) on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
