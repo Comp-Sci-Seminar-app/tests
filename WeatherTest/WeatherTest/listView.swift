3e2548fba2311900d3db068360bd8a28aeb0e109
Ari Steinfeld <crazybonesx@gmail.com>
2022-01-04 08:30:14 -0500

    end of day commit

---
+++ WeatherTest/WeatherTest/listView.swift
@@ -0,0 +1,16 @@
+//
+//  listView.swift
+//  WeatherTest
+//
+//  Created by Ari Steinfeld (student LM) on 1/4/22.
+//
+
+import SwiftUI
+
+struct listView: View {
+    @Binding var forc : Forcast
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
