c8e943beb7c80b7457e99c968cf952fc1888a0fd
Ari Steinfeld <crazybonesx@gmail.com>
2022-01-06 08:26:48 -0500

    1 comment

--- WeatherTest/WeatherTest/listView.swift
+++ WeatherTest/WeatherTest/listView.swift
@@ -12,7 +12,8 @@ struct listView: View {
     
     var h : Hour
   
-    //basic view for when its in the list
+    //right here is where the people on stack overflow were a**holes. they told me that my issue was related to an unrelated issue, and NOT because my code was simply in the wrong place. They also lowered my community score so now any time I want to post it has to get reviewed
+    
     var body: some View {
         let rawTime = h.time
         HStack{
