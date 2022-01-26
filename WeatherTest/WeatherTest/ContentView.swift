4120e1a7909f07fdd1e5b502afa68999c0cf7d31
Ari Steinfeld <crazybonesx@gmail.com>
2022-01-26 09:33:07 -0500

    changed some comments

--- WeatherTest/WeatherTest/ContentView.swift
+++ WeatherTest/WeatherTest/ContentView.swift
@@ -208,7 +208,7 @@ struct ContentView: View {
             }
             
         )
-        //test
+        
         
     }
 }
--- WeatherTest/WeatherTest/listView.swift
+++ WeatherTest/WeatherTest/listView.swift
@@ -17,6 +17,8 @@ struct listView: View {
   
     //right here is where the people on stack overflow were a**holes. they told me that my issue was related to an unrelated issue, and NOT because my code was simply in the wrong place. They also lowered my community score so now any time I want to post it has to get reviewed
     
+    //sorry for bad language but its true!
+    
     var body: some View {
         
         //for some reason the overlords at weatherapi.com forgot to add https to their image links. this is why the link looks so weird
