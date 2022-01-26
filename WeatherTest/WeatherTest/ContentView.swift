e0d272206cf72875680e23aac184b968af5c417c
Samuel Conry-Murray <finseyconrymurray@gmail.com>
2022-01-26 10:41:12 -0500

    new api

--- WeatherTest/WeatherTest/ContentView.swift
+++ WeatherTest/WeatherTest/ContentView.swift
@@ -4,32 +4,40 @@
 //
 //  Created by Ari Steinfeld (student LM) on 12/16/21.
 //
-
+func URlForm(_ address : String = "618 Schiller Ave", city : String = "Merion Station", state : String = "PA", zip : String = "19066") -> String{//function reads in an address and outputs a string url
+    var finalURL  = "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address="//static portion of the URL that holds the paramaters we dont need to change
+    let URLEnd = "&benchmark=2020&format=json"//same as above but we stick it on the back
+    finalURL = finalURL+address.replacingOccurrences(of: " ", with: "+")+"%2C+"+city.replacingOccurrences(of: " ", with: "+")+"%2C+"+state+"+"+zip+URLEnd//creates the URL replacing spaces with plus signs and adding other things in between that are nessecary for the link to function
+    //print("In URLForm: "+finalURL)//prints for debug
+    return finalURL//returns the url
+}
 import SwiftUI
 import struct Kingfisher.KFImage
 
 
 struct ContentView: View {
+    @StateObject var g = Decoded()
     @StateObject var f = FetchData()
     //sam's branch
     //makes the back button invisible so I can add a custom one.
     init(){
-           Theme.navigationBarColors(background: .clear, titleColor: .clear)
-        }
+        Theme.navigationBarColors(background: .clear, titleColor: .clear)
+    }
     
     var body: some View {
-        
+        let allDaily = g.dForecast.properties.periods
+        let allHourly = g.hForecast.properties.periods
         NavigationView{
             
-            ScrollView{
+            VStack{//Ask why this was a scrollview
                 
                 
                 Group{
                     VStack{
                         //just the current stuff
                         Text("Current Forecast")
-                        Text("condition: \(f.responses.current.condition?.text ?? "Loading...")")
-                        Text("temp: \(Int(Double.rounded(f.responses.current.temp_f)())) degrees fahrenheit")
+                        Text("condition: \(allHourly[0].shortForecast ?? "Loading...")")
+                        Text("temp: \(allHourly[0].temperature) degrees fahrenheit")
                     }
                     .frame(width: UIScreen.main.bounds.width - 30, height: 120, alignment: .center)
                     .background(Color.gray.opacity(0.8))
@@ -44,12 +52,12 @@ struct ContentView: View {
                         LazyHStack {
                             ForEach(0..<f.responses.forecast.forecastday[0].hour.count){index in
                                 NavigationLink(destination:
-                                                detailedView(info: f.responses.forecast.forecastday[0].hour[index])
+                                                detailedView(info: allHourly[index], icon: f.responses.forecast.forecastday[0].hour[index])
                                                 .edgesIgnoringSafeArea(.all), //allows it to extend into the navigation bar area
                                                label:  {
                                                 listView(h: f.responses.forecast.forecastday[0].hour[index])
-                                                .frame(width: 90, height: 100, alignment:  .center).background(Color.gray).opacity(0.4).cornerRadius(20)})
-                                                //frames it to look nice and modern rather than what the actual code is.
+                                                    .frame(width: 90, height: 100, alignment:  .center).background(Color.gray).opacity(0.4).cornerRadius(20)})
+                                //frames it to look nice and modern rather than what the actual code is.
                                 
                                 
                                 
@@ -57,15 +65,16 @@ struct ContentView: View {
                         }
                         //makes it look much nicer
                     }.navigationBarHidden(true)
+                    
                 }
-                
+                Spacer()
                 
                 
                 //also needed here or else it gets angry, and sets a good background
             }.navigationBarHidden(true).background(
                 Group{
                     //checks if it is night
-                    if (timeToInt(f.responses.location.localtime) < 19 && timeToInt(f.responses.location.localtime) > 5){
+                    if allHourly[0].isDaytime{
                         Image("\(f.responses.current.condition?.code ?? 1000)")
                             .resizable()
                             .aspectRatio(contentMode: .fill)
@@ -88,7 +97,7 @@ struct ContentView: View {
         } .background(
             Group{
                 //checks if it is night
-                if (timeToInt(f.responses.location.localtime) < 19 && timeToInt(f.responses.location.localtime) > 5){
+                if allHourly[0].isDaytime{
                     Image("\(f.responses.current.condition?.code ?? 1000)")
                         .resizable()
                         .aspectRatio(contentMode: .fill)
@@ -103,6 +112,7 @@ struct ContentView: View {
             
         )
         //test
+
         
     }
 }
@@ -127,13 +137,14 @@ func timeToInt(_ rawTime : String) -> Int {
 //making a header for the code
 struct ListHeader: View {
     @Binding var f: Response
+    @Binding var g: HPeriods
     var body: some View {
         HStack {
             VStack{
                 //just the current stuff
                 Text("Current Forecast")
                 Text("condition: \(f.current.condition?.text ?? "Loading...")")
-                Text("temp: \(Int(Double.rounded(f.current.temp_f)())) degrees fahrenheit")
+                Text("temp: \(g.temperature) degrees fahrenheit")
             }
             .frame(width: UIScreen.main.bounds.width - 30, height: 120, alignment: .center)
             .background(Color.gray.opacity(0.8))
@@ -162,6 +173,7 @@ class Theme {
         let navigationAppearance = UINavigationBarAppearance()
         navigationAppearance.configureWithOpaqueBackground()
         navigationAppearance.backgroundColor = background ?? .black
+        
         
         navigationAppearance.titleTextAttributes = [.foregroundColor: titleColor ?? .black]
         navigationAppearance.largeTitleTextAttributes = [.foregroundColor: titleColor ?? .black]
